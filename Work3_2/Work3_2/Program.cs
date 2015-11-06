0bb0d6fbd982cef58a04fa9ce2ea09af10fa2db5
建 彭 <pengjian@live.cn>
2015-11-06 23:22:34 +0800

    Work3_2

---
+++ Work3_2/Work3_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入所要到的数:");
+            int i;
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("从0到{0}:{1}", i, way(i));
+        }
+        static string way(int y, int x = 0)
+        {
+            int[] a;
+            a = new int[10] { -1, 0, 0, 1, 2, 4, 3, 3, 5, 6 };
+            if (y == 0)
+            {
+                return "0";
+            }
+            else
+            {
+                return way(a[y]) + " " + y;
+            }
+        }
+    }
+}
